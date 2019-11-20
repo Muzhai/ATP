2fe6a6a7a27c5a09c334cbbbc939d2d04fef9460
muzhai <limuzhai@outlook.com>
2019-11-20 22:50:10 +0100

    add creat_table, insert_table

--- Baum/SQL.py
+++ Baum/SQL.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
-#hallo this is a test
+host='127.0.0.1'        #defaul IP adress
+user='ATPbaum'
+password='ATPbaum'
+database='Baum'
+# creat a connection to the database
+conn = pymssql.connect(host, user, password, database, charset='utf8')
+cursor = conn.cursor()
+
+
+
 
--- Baum/funktion.py
+++ Baum/funktion.py
@@ -10,7 +10,7 @@ def create_table(table_name, cursor):
     IF OBJECT_ID('%s', 'U') IS NOT NULL
         DROP TABLE %s
     CREATE TABLE %s (
-        N VARCHAR(10) NOT NULL,
+        index VARCHAR(10) NOT NULL,
         GPS VARCHAR(10),
         date varchar(10),
         PRIMARY KEY(N))
@@ -19,9 +19,23 @@ def create_table(table_name, cursor):
     cursor.execute(sql)
 
 
-def insert_table(table_name, cursor, n, GPS, date):
-    sql = "insert into %s values('%s', '%s', '%s')" %(table_name, n, GPS , date )
+def insert_table(table_name, cursor, index, GPS, date):
+    sql = "insert into %s values('%s', '%s', '%s')" %(table_name, index, GPS , date )
     cursor.execute(sql)
+
+
+def drop_table(table_name, cursor):
+    #  complete delete
+    sql = "drop table %s" %(table_name)
+    cursor.execute(sql)
+    conn.commit()
+
+
+def delete_row(table_name, cursor, row_number='' )
+    if row_number is NULL
+        sql = "delete Nr=max(Nr) from %s where Nr=max(Nr)" % (table_name)
+
+
 
 
 
