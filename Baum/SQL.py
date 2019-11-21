8bf2bc1649eddac2b7e8ebe9366bfb1fba1d71c0
muzhai <limuzhai@outlook.com>
2019-11-21 16:00:28 +0100

    add creat_table, insert_table

--- Baum/SQL.py
+++ Baum/SQL.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+import pymssql
 
 host='127.0.0.1'        #defaul IP adress
 user='ATPbaum'
@@ -9,6 +10,24 @@ database='Baum'
 conn = pymssql.connect(host, user, password, database, charset='utf8')
 cursor = conn.cursor()
 
+table_name='hallo675'
+sql = "create table %s ( index varchar(10), GPS varchar(10))" %(table_name)
+
+print(sql)
+cursor.execute(sql)
+
+# sql1 = "create table %s ( id varchar(10), GPS varchar(5))" %(table_name)
+# print(sql1)
+# cursor.execute(sql1)  # 执行Sql语句
 
 
 
+# resList = cursor.fetchall()  # 获得所有的查询结果
+# print(resList)
+
+
+# sql1 = "\"create table halloopp ( id varchar(10), GPS varchar(5))\" "
+# print(sql1)
+
+# cursor.execute(sql1)
+# conn.commit()
--- Baum/funktion.py
+++ Baum/funktion.py
@@ -1,41 +1,50 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
-# 新建表
-# 先创建光标
 
 
-def create_table(table_name, cursor):
+def main(server, user, password, database):
+    import Class
+    global mssql
+    mssql = Class.MSSQL(server, user, password, database)
+    return mssql
+
+
+
+
+def create_table(table_name):
+
     sql = """
     IF OBJECT_ID('%s', 'U') IS NOT NULL
         DROP TABLE %s
     CREATE TABLE %s (
-        index VARCHAR(10) NOT NULL,
+        id VARCHAR(10) NOT NULL,
         GPS VARCHAR(10),
         date varchar(10),
-        PRIMARY KEY(N))
+        PRIMARY KEY(id))
     """ %(table_name, table_name, table_name)
+    mssql.exec_non_query(sql)
 
-    cursor.execute(sql)
+
+def drop_table(table_name):
+    #  complete delete
+    sql = "drop table %s" % (table_name)
+    mssql.exec_non_query(sql)
 
 
 def insert_table(table_name, cursor, index, GPS, date):
     sql = "insert into %s values('%s', '%s', '%s')" %(table_name, index, GPS , date )
-    cursor.execute(sql)
+    mssql.exec_non_query(sql)
 
 
-def drop_table(table_name, cursor):
-    #  complete delete
-    sql = "drop table %s" %(table_name)
-    cursor.execute(sql)
-    conn.commit()
 
 
-def delete_row(table_name, cursor, row_number='' )
-    if row_number is NULL
-        sql = "delete Nr=max(Nr) from %s where Nr=max(Nr)" % (table_name)
 
 
+
+# def delete_row(table_name, cursor, row_number='' )
+#     if row_number is NULL
+#         sql = "delete Nr=max(Nr) from %s where Nr=max(Nr)" % (table_name)
 
 
 
