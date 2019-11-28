1397c903b59e9ecf8eafb88078c329c2622c9b11
muzhai <limuzhai@outlook.com>
2019-11-28 15:48:39 +0100

    根据Id删除行

--- Baum/funktion.py
+++ Baum/funktion.py
@@ -2,6 +2,7 @@
 # _*_ coding:utf-8 _*_
 
 
+# 创建一个数据库连接
 def main(server, user, password, database):
     import Class
     global mssql
@@ -9,7 +10,8 @@ def main(server, user, password, database):
     return mssql
 
 
-def create_table(table_name):       # 创建一个定值表
+# 创建一个定值表
+def create_table(table_name):
 
     sql = """
     IF OBJECT_ID('{0}', 'U') IS NOT NULL
@@ -21,6 +23,57 @@ def create_table(table_name):       # 创建一个定值表
         date varchar(10))
     """.format(table_name)
     mssql.exec_non_query(sql)
+
+
+# 删除整个列表
+def drop_table(table_name):
+    #  complete delete
+    sql = "drop table {table_name}" .format(table_name=table_name)
+    mssql.exec_non_query(sql)
+
+
+# 向默认baum_test表插值，输入格式为一个字典
+def insert_table_baum_test(baum, table_name='baum_test'):
+    sql = """
+    insert into {table_name}(tag_id, device_id, GPS, date) 
+    select '{D[tag_id]}', ' {D[device_id]}', ' {D[GPS]}', '{D[date]}'
+    """.format(D=baum, table_name=table_name)
+    mssql.exec_non_query(sql)
+
+
+# 批量向固定表格插入固定格式数据数据 输入参数为列表格式
+def insert_table_batch(baum_list, table_name='baum_test'):
+    for baum in baum_list:
+        insert_table_baum_test(baum, table_name)
+
+
+# 输出整个表
+def query_table(table_name):
+    sql = "select * from {table_name}".format(table_name=table_name)
+    result=mssql.exec_query(sql)
+    print(result)
+
+
+# 按tag_id筛选并按日期排序
+def query_table_id(tag_id):
+    sql = """
+        select * from baum_test 
+        where tag_id = '{0}' 
+        order by date""" .format(tag_id)
+    result = mssql.exec_query(sql)
+    print(result)
+
+
+# 删除某id下的全部行
+def delete_table_id(tag_id):
+    sql = """
+    delete from baum_test 
+    where tag_id ='{0}'
+    """ .format(tag_id)
+    mssql.exec_non_query(sql)
+
+
+
 
 
 # def create_table1(baum):        # 根据字典创建列表
@@ -38,10 +91,10 @@ def create_table(table_name):       # 创建一个定值表
 #     mssql.exec_non_query(sql)
 
 
-def drop_table(baum):       # 删除整个列表
-    #  complete delete
-    sql = "drop table {table_name}" .format(table_name=baum['tag_id'])
-    mssql.exec_non_query(sql)
+# def drop_table(baum):       # 删除整个列表
+#     #  complete delete
+#     sql = "drop table {table_name}" .format(table_name=baum['tag_id'])
+#     mssql.exec_non_query(sql)
 
 
 # def insert_table(baum):     # 向列表插入值 列表名为tag_id
@@ -52,61 +105,36 @@ def drop_table(baum):       # 删除整个列表
 #     mssql.exec_non_query(sql)
 
 
-def insert_table_baum_test(baum, table_name='baum_test'):     # 向默认baum_test表插值
-    sql = """
-    insert into {table_name}(tag_id, device_id, GPS, date) 
-    select '{D[tag_id]}', ' {D[device_id]}', ' {D[GPS]}', '{D[date]}'
-    """.format(D=baum, table_name=table_name)
-    mssql.exec_non_query(sql)
 
-
-def insert_table_batch(baum_list, table_name='baum_test'):              # 批量向固定表格插入固定格式数据数据
-    for baum in baum_list:
-        insert_table_baum_test(baum, table_name)
+# def query_table(baum):
+#     sql = "select * from {D[tag_id]}".format(D=baum)
+#     result=mssql.exec_query(sql)
+#     print(result)
 
 
 
-def query_table(baum):
-    sql = "select * from {D[tag_id]}".format(D=baum)
-    result=mssql.exec_query(sql)
-    print(result)
 
 
-def query_table1(table_name):
-    sql = "select * from {table_name}".format(table_name=table_name)
-    result=mssql.exec_query(sql)
-    print(result)
-
-
-def query_table_id(tag_id):     # 按tag_id筛选并按日期排序
-    sql = """
-        select * from baum_test 
-        where tag_id = '{0}' 
-        order by date""" .format(tag_id)
-    result=mssql.exec_query(sql)
-    print(result)
-
-
-def creat_insert_table(baum):       # 根据字典创建并插入值
-    li=['tag_id', 'device_id', 'GPS', 'date']       # 事先约定好表格的列名
-    sql = """
-    IF OBJECT_ID('{D[tag_id]}', 'U') IS NOT NULL
-        insert into {D[tag_id]}(tag_id, device_id, GPS, date) 
-        select '{D[tag_id]}',' {D[device_id]}',' {D[GPS]}', '{D[date]}'
-    ELSE
-        begin
-            CREATE TABLE {D[tag_id]} (
-                Num int identity(1,1), 
-                {K[0]} VARCHAR(10) NOT NULL,
-                {K[1]} VARCHAR(10),
-                {K[2]} VARCHAR(10),
-                {K[3]} varchar(10))
-            insert into {D[tag_id]}(tag_id, device_id, GPS, date) 
-            select '{D[tag_id]}',' {D[device_id]}',' {D[GPS]}', '{D[date]}'
-        end
-    """.format(table_name=baum['tag_id'], K=li, D=baum)
-    mssql.exec_non_query(sql)
-
+# def creat_insert_table(baum):       # 根据字典创建并插入值
+#     li=['tag_id', 'device_id', 'GPS', 'date']       # 事先约定好表格的列名
+#     sql = """
+#     IF OBJECT_ID('{D[tag_id]}', 'U') IS NOT NULL
+#         insert into {D[tag_id]}(tag_id, device_id, GPS, date)
+#         select '{D[tag_id]}',' {D[device_id]}',' {D[GPS]}', '{D[date]}'
+#     ELSE
+#         begin
+#             CREATE TABLE {D[tag_id]} (
+#                 Num int identity(1,1),
+#                 {K[0]} VARCHAR(10) NOT NULL,
+#                 {K[1]} VARCHAR(10),
+#                 {K[2]} VARCHAR(10),
+#                 {K[3]} varchar(10))
+#             insert into {D[tag_id]}(tag_id, device_id, GPS, date)
+#             select '{D[tag_id]}',' {D[device_id]}',' {D[GPS]}', '{D[date]}'
+#         end
+#     """.format(table_name=baum['tag_id'], K=li, D=baum)
+#     mssql.exec_non_query(sql)
+#
 
 
 
